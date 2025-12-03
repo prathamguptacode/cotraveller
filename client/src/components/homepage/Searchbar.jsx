478f1ff86b53a290328b7d678f00610165ae1610
Pratham Gupta <prathamgupta.wk@gmail.com>
2025-12-03 16:56:18 +0530

    some frontend

--- client/src/components/homepage/Searchbar.jsx
+++ client/src/components/homepage/Searchbar.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { useTheme } from '../../hooks/useTheme'
+import mystyle from './Searchbar.module.css'
 
 function Searchbar() {
     const {theme , toggleTheme} = useTheme()
+    //college
+    //transport
+    //date
+    //timerange
   return (
-    <div>
-      
+    <div className={mystyle.searchbar}>
+      <div className={mystyle.inbx}>
+      </div>
     </div>
   )
 }
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../components/homepage/Navbar'
 import Banner from '../../components/homepage/Banner'
+import Searchbar from '../../components/homepage/Searchbar'
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Searchbar></Searchbar>
       This is home, i love it hello ji namaste
     </div>
   )
