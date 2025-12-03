9f9d7d8729c18bfbb1fa8988144bedfd901ac8d7
Pratham Gupta <prathamgupta.wk@gmail.com>
2025-12-03 13:55:28 +0530

    some frontend

--- client/src/components/homepage/Navbar.jsx
+++ client/src/components/homepage/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import mystyle from './Navbar.module.css'
 import { GiHamburgerMenu } from "react-icons/gi";
+import ThemeButton from '../Buttons/ThemeButton';
+import { IoIosMail } from "react-icons/io";
 
 function Navbar() {
   return (
     <div className={mystyle.navbarWrapper}>
       <div className={mystyle.navbar}>
-        <GiHamburgerMenu size='36px' color='black'></GiHamburgerMenu>
-        <div className={mystyle.login}></div>
+        <GiHamburgerMenu size='36px' />
+        <div className={mystyle.logo}>
+          Cotraveller
+        </div>
+        <div className={mystyle.dummybx}></div>
+        <ThemeButton size='28px'/>
+        <IoIosMail size='28px' />
+        <button className={mystyle.createBtn}>Create Group</button>
+        <button className={mystyle.signup}>Sign up</button>
+        <button className={mystyle.login}>Login in</button>
       </div>
     </div>
   )
