2506c9d30ec1936cc481aca428ebbbf95b5cdc0b
Pratham Gupta <prathamgupta.wk@gmail.com>
2025-12-07 15:52:42 +0530

    toasts

---
+++ client/src/components/homepage/TrustCan.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import mystyle from './Homepage.module.css'
+
+function TrustCan() {
+    return (
+        <div className={mystyle.trustbox}>
+            
+        </div>
+    )
+}
+
+export default TrustCan
