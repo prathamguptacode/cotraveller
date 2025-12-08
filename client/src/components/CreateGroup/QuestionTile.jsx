a55b470a6cbfc3996edc89449be456aa2c862749
Shubham1823L <shubhuisbetter@gmail.com>
2025-12-08 13:02:02 +0530

    what

--- client/src/components/CreateGroup/QuestionTile.jsx
+++ client/src/components/CreateGroup/QuestionTile.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import mystyle from './CreateGroup.module.css'
+import mystyle from './creategroup.module.css'
 import { IoMdAirplane } from "react-icons/io";
 import { FaTrainSubway } from "react-icons/fa6";
 import { FaTaxi } from "react-icons/fa";
