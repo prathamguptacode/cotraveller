97da132286acfbeea4a95e25df6e0fd1d35394f0
Shubham1823L <shubhuisbetter@gmail.com>
2026-03-18 12:36:22 +0530

    added profile dialog ui for user (self use only)

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -5,9 +5,8 @@ import { Link } from 'react-router-dom';
 import { useEffect, useRef, useState, type ReactNode } from "react";
 import Groups from "@/components/SidebarTabs/Groups";
 import Inbox from "@/components/SidebarTabs/Inbox";
-import { Inbox as InboxLogo, Plus, TextAlignJustify } from "lucide-react";
+import { Camera, Edit, Inbox as InboxLogo, Plus, TextAlignJustify, User2Icon, X } from "lucide-react";
 import { useAuth } from "@/hooks/useAuth";
-import { FaUser } from "react-icons/fa6";
 import clsx from "clsx";
 import { useEventSource } from "@/hooks/useEventSource";
 import { toast } from 'sonner';
@@ -146,15 +145,89 @@ Navbar.CreateGroupButton = NavbarCreateGroupButton
 
 const NavbarProfileButton = () => {
     const { user } = useAuth()
+    const dialogRef = useRef<HTMLDialogElement>(null)
 
     const url = user?.avatar.publicId && getImgURL(user.avatar.publicId, user.avatar.version, 400)
     const firstLetter = user?.fullName.charAt(0)
 
+    const openDialog = () => {
+        const dialog = dialogRef.current
+        if (!dialog) return
+        dialog.showModal()
+    }
 
-    return user && <Link className={mystyle.avatarWrapper} aria-label="Go to your profile page" to={`/`}>
-        {url ? <img src={url} alt="user-avatar" /> : firstLetter}
-    </Link>
+    const closeDialog = () => {
+        const dialog = dialogRef.current
+        if (!dialog) return
+        dialog.close()
+    }
 
+    useEffect(() => {
+        const dialog = dialogRef.current
+        if (!dialog) return
+
+        const eventHandler = (e: PointerEvent) => {
+            const dialogDimensions = dialog.getBoundingClientRect()
+            if (e.clientX < dialogDimensions.left || e.clientX > dialogDimensions.right || e.clientY < dialogDimensions.top || e.clientY > dialogDimensions.bottom) dialog.close()
+        }
+        dialog.addEventListener('click', eventHandler)
+
+        return () => {
+            dialog.removeEventListener('click', eventHandler)
+        }
+    }, [])
+
+    const formattedDate = user && new Intl.DateTimeFormat('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+    }).format(new Date(user.createdAt))
+
+    return user &&
+        <>
+            <button onClick={openDialog} className={mystyle.avatarWrapper} aria-label="View your profile">
+                {url ? <img src={url} alt="user-avatar" /> : firstLetter}
+            </button>
+            <dialog ref={dialogRef} className={mystyle.profileDialog}>
+                <button className={mystyle.closeDialog} onClick={closeDialog}><X strokeWidth={1.5} size={20} /></button>
+                <div className={mystyle.profileArea}>
+                    <div className={mystyle.profileHeader}>
+                        <div className={mystyle.avatarWrapper}>
+                            {url ? <img src={url} alt="user-avatar" /> : firstLetter}
+                        </div>
+                        <div className={mystyle.profileHeaderDetails}>
+                            <h2>{user.username}</h2>
+                            <span>{user.fullName}</span>
+                        </div>
+                    </div>
+                    <div className={mystyle.profileMainArea}>
+                        <div>
+                            <h3>Email</h3>
+                            <span>{user.email}</span>
+                        </div>
+                        <div>
+                            <h3>Member Since</h3>
+                            <span>
+                                {formattedDate}
+                            </span>
+                        </div>
+                        <div>
+                            <h3>Associated Groups</h3>
+                            <span>{user.memberGroup.length}</span>
+                        </div>
+                    </div>
+                    <div className={mystyle.profileFooter}>
+                        <Link to={'#'} >
+                            <Edit size={20} /> Edit Profile
+                        </Link>
+                        <button>
+                            <Camera size={20} /> Change Photo
+                        </button>
+                    </div>
+                </div>
+
+            </dialog>
+        </>
 }
 
 Navbar.ProfileButton = NavbarProfileButton
